d067c5a5ac9c66fd4968f49fe6486cd435782237
Aleksandr Krays <krais1234@gmail.com>
2022-01-11 17:59:40 +0700

    Добавил axios.
    Добавил методы для работы с апи.

---
+++ src/api/todos.ts
@@ -0,0 +1,22 @@
+import { API } from './axiosConfig';
+import { TodoItem, TodoItemStructure } from '../components/todo-items-list/todo-items-list';
+
+export const getAllTodo = () => {
+  return API.get<TodoItem[]>('/todos');
+};
+
+export const createTodo = ({ name, description, isDone }: TodoItemStructure) => {
+  return API.post<TodoItemStructure>('/todo/create', {
+    name,
+    description,
+    isDone,
+  });
+};
+
+export const updateTodo = ({ name, description, isDone, _id }: TodoItem) => {
+  return API.put<TodoItemStructure>(`/todo/update/${ _id }`, {
+    name,
+    description,
+    isDone,
+  });
+};
